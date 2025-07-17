11c61d6f70e9c6caf1fa060da568b9c726499bb9
paulina-pacheco-o <paulimichi05@gmail.com>
2025-07-17 15:31:54 +0200

    snack 5

--- js-jsnacks-blocco-5/snack-5/script.js
+++ js-jsnacks-blocco-5/snack-5/script.js
@@ -2,3 +2,13 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+const pari = nums.filter((numeri) => {
+  if (numeri % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+})
+
+console.log(pari)
