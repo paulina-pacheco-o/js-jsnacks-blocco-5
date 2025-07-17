cddb207c26a940039ba6c3b2fc54b09d9e3ef006
paulina-pacheco-o <paulimichi05@gmail.com>
2025-07-17 16:16:07 +0200

    snack 8

--- js-jsnacks-blocco-5/snack-1/script.js
+++ js-jsnacks-blocco-5/snack-1/script.js
@@ -2,6 +2,6 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
-names.forEach((element) => {
-  console.log(element);
+names.forEach((name) => {
+  console.log(name);
 })
--- js-jsnacks-blocco-5/snack-8/script.js
+++ js-jsnacks-blocco-5/snack-8/script.js
@@ -21,3 +21,9 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+const sezione = students.find((studente) => {
+  return studente.name === "Marco Lanci"
+})
+
+console.log(sezione.class)
